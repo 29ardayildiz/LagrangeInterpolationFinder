fb4750f9ba74901b754bafee1ed7a55aaded9a42
Arda YILDIZ <ardayildiz029@gmail.com>
2024-05-29 15:14:16 +0300

    Third commit

--- LagrangeInterpolationFinder.cpp
+++ LagrangeInterpolationFinder.cpp
@@ -1,40 +1,37 @@
 ﻿#include <iostream>
 #include <vector>
-#include <sstream>
+#include <string>
 
 using namespace std;
 
-string generateLagrangePolynomial(const vector<int>& x, const vector<int>& y) {
+string LPolynomial(const vector<int>& x, const vector<int>& y) {
     int n = x.size();
-    stringstream polynomial;
-
-    polynomial << "P(x) = ";
+    string polynomial = "P(x) = ";
 
     for (int i = 0; i < n; ++i) {
-        double yi = y[i];
-        if (i > 0) polynomial << " + ";
-        polynomial << yi << " * ";
+        int yi = y[i];
+        if (i > 0) polynomial += " + ";
+        polynomial += to_string(yi) + " * ";
 
         // L_i(x)
-        stringstream LiNumerator;
-        stringstream LiDenominator;
+        string Ln = "";
+        string Ld = "(";
 
-        LiDenominator << "(";
         for (int j = 0; j < n; ++j) {
             if (j != i) {
-                if (LiNumerator.str().length() > 0) LiNumerator << " * ";
-                if (LiDenominator.str().length() > 1) LiDenominator << " * ";
+                if (!Ln.empty()) Ln += " * ";
+                if (Ld.length() > 1) Ld += " * ";
 
-                LiNumerator << "(x - " << x[j] << ")";
-                LiDenominator << "(" << x[i] << " - " << x[j] << ")";
+                Ln += "(x - " + to_string(x[j]) + ")";
+                Ld += "(" + to_string(x[i]) + " - " + to_string(x[j]) + ")";
             }
         }
-        LiDenominator << ")";
+        Ld += ")";
 
-        polynomial << "[" << LiNumerator.str() << "] / " << LiDenominator.str();
+        polynomial += "[" + Ln + "] / " + Ld;
     }
 
-    return polynomial.str();
+    return polynomial;
 }
 
 int main() {
@@ -43,12 +40,12 @@ int main() {
     cin >> n;
 
     vector<int> x(n), y(n);
-    cout << "Enter the x and y values for each point:" << endl;
+    cout << "Enter the x and y values :" << endl;
     for (int i = 0; i < n; ++i) {
         cin >> x[i] >> y[i];
     }
 
-    string polynomial = generateLagrangePolynomial(x, y);
+    string polynomial = LPolynomial(x, y);
     cout << polynomial << endl;
 
     return 0;
